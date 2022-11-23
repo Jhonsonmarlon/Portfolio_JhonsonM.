72ad929d88cd68c9d5e2c41d001c52d21d494d70
jhonsonmarlon <silva123jms@gmail.com>
2022-11-23 10:32:20 -0400

    :tada: Build 3.0 (responsividade e links)

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 function clickMenu() {
     if (itens.style.display == 'flex') {
-        itens.style.display = 'none'
+        itens.style.display = 'none';
     } else {
-        itens.style.display = 'flex'
+        itens.style.display = 'flex';
     }
 }
