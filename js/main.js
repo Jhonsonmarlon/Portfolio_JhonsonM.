966d731676bc84ee088febe312ddea5a40454376
jhonsonmarlon <silva123jms@gmail.com>
2022-12-01 12:12:28 -0400

    possíveis alterações

--- js/main.js
+++ js/main.js
@@ -13,3 +13,17 @@ function clickForaMenu() {
         itens.style.display = 'none';
     }
 }
+
+function clickWork(){
+    if (op1.style == ':hover'){
+        web_works.style.display = 'flex';
+        design_works.style.display = 'flex';
+    } else if (op2.style == ':hover'){
+        web_works.style.display = 'flex';
+        design_works.style.display = 'none';
+    } else if (op3.style == ':hover'){
+        web_works.style.display = 'none';
+        design_works.style.display = 'flex';
+    }
+}
+
