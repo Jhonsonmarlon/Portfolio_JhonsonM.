a822e19054427d600817a369ee920f1845eb5a0b
jhonsonmarlon <silva123jms@gmail.com>
2022-12-01 09:47:22 -0400

    correção de bugs e links

--- js/main.js
+++ js/main.js
@@ -5,3 +5,19 @@ function clickMenu() {
         itens.style.display = 'flex';
     }
 }
+
+function clickForaMenu() {
+    if (itens.style.display == 'flex') {
+        itens.style.display = 'none';
+    } else {
+        itens.style.display = 'none';
+    }
+}
+
+function clickOp() {
+    if (item.style.display == 'active') {
+        itens.style.display = 'none';
+    } else {
+        itens.style.display = 'flex';
+    }
+}
